bb7a6bfc3ea481ef7caa4e1bd2c615443dfca22f
John Tynan <jgtynan@gmail.com>
2015-01-16 08:29:40 +0800

    Update chceck_links.js

--- chceck_links.js
+++ chceck_links.js
@@ -2,53 +2,116 @@
 
 // alert(location.host);
 
-
-var newDiv = document.createElement('div');
-document.body.appendChild(newDiv);
-newDiv.id = 'link_test'
-newDiv.style.width = "500px"; 
-newDiv.style.height = "500px"; 
-newDiv.style.background = "#77a4d4"; 
-newDiv.style.borderStyle = "solid"; 
-newDiv.style.borderWidth = "1px";
-
+var pageTitle = document.title
 var links = document.getElementsByTagName('a'); // get all links
+var totalLinks = links.length
+var linkAnchors = document.getElementsByTagName('a');
+var linkInfo = ''
 
-alert('Link Test Started');
 
-for (var i = 0 ; i < links.length; i++)
-{
+var w = window.open();
+w.document.open();
+w.document.write('<h1></h1>');
+w.document.title = 'Link Test for ' + pageTitle
+// w.document.close();
 
-  // alert(links[i].href);
+var linksTested = 0;
+// alert(isNaN(linksTested));
 
-  if (links[i].host !== location.host) {
-  // external
-  }
-  else {
-  // internal
+var testStatus = w.document.createElement('div');
+var testProgress = w.document.createElement('div');
 
-    // alert(links[i].host);
+function pause(){
+  //do some things
+  setTimeout(continueExecution, 3000) //wait 3 seconds before continuing
+}
 
-    url = links[i].href;
 
-    // alert(url);
+// begin contintueExecution function here
+function continueExecution(){
 
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
+  w.document.body.appendChild(testStatus);
+  w.document.body.appendChild(testProgress);
 
-    if (http.status != 200){
+  testProgress.style.width = "100%";
+
+  testStatus.innerHTML = '<h1>Link Test Started</h1>'
+
+  testProgress.textContent = 'Total links: ' + totalLinks 
+
+  // begin for loop over links here
+  for (var i = 0 ; i < links.length; i++)
+  {
+
+    // alert(links[i].href);
+
+    // begin for loop to test internal / external links here
+    if (links[i].host !== location.host) {
+      // external
+    }
+    else {
+      // internal
+
+      // alert(links[i].host);
+
+      url = links[i].href;
+
       // alert(url);
+
+        // begin switch statement to elimintate exclusions
+        str = url;
+
+        switch (str) {
+          case str.match(/layouts/gi):
+            console.log("Matched a url that contains 'layouts'");
+            break;
+          case str.match(/file/gi):
+            console.log("Matched a url that contains 'file'");        
+            break;
+          default:
+            console.log("this one is okay to test");
+            // begin if loop to test http status
+            // if (http.status != 200){
+            //  the next line is some dummy code for testing purposes. Comment out in production
+            if (i == i){
+
+              // alert(url);
+              // var http = new XMLHttpRequest();
+              // http.open('HEAD', url, false);
+              // http.send();
+
+              linkInfo = linkInfo + i.toString() + ') ' + links[i].href + ' = ' + linkAnchors[i].text + '<br />'
+              var linksTested = parseInt(linksTested);
+              var linksTested = linksTested++;
+            
+            // end for loop to test http status
+            }
+
+            break;
+        
+        // end switch statement to elimintate exclusions
+        }
+
+    // end for loop to test internal / external links here
     }
 
-  newDiv.textContent = i;
-
+  // end for loop over links here
   }
+
+
+testStatus.innerHTML = '<h1>Link Test Finished for: ' + pageTitle + '</h1>'
+
+// alert(linksTested);
+
+// testProgress.innerHTML = 'Total Links: ' + totalLinks + '<br /> Links Tested: ' + linksTested.toString() + '<br />'
+
+// testProgress.textContent = linkInfo
+
+testProgress.innerHTML = linkInfo
 
 }
 
-alert('Link Test Finished');
-
+pause();
 
 /*
 // bookmarklet code
